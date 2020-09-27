a5865d5d487f77c9df04492ef334e285c8bef7e8
Keisuke Awazu <awakei@KeisukenoMacBook-Pro.local>
2020-09-27 22:58:37 +0900

    q3 fin

--- ruby_puzzle/q_3.rb
+++ ruby_puzzle/q_3.rb
@@ -1,7 +1,14 @@
 N = 100
-array = Array.new(100, false)
+array = Array.new(N, false)
 
-n = 2
-while n < N do
-  array[n] = 
+(2..N).each do |n|
+  i = n
+  while i < array.size do
+    array[i - 1] = !array[i - 1]
+    i += n
+  end
+end
+
+array.each.with_index(1) do |v, i|
+  puts i unless v
 end
